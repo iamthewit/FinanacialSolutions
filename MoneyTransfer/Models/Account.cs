a0a6d6794a3c49709f3ec0692ec734f12e43bdb7
iamthewit <bencross86@gmail.com>
2025-07-09 17:05:48 +0100

    Adding validation and tests

--- MoneyTransfer/Models/Account.cs
+++ MoneyTransfer/Models/Account.cs
@@ -5,14 +5,25 @@ namespace MoneyTransfer.Models;
 public class Account
 {
     public int Id { get; set; }
+    
+    [Required]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Account balance must be greater than zero.")]
     public decimal AccountBalance { get; set; }
+    
     [Required]
     [MinLength(1, ErrorMessage = "Full name cannot be empty.")]
     public required string FullName { get; set; }
+    
+    [Required]
+    [EnumDataType(typeof(AccountType), ErrorMessage = "Invalid account type.")]
     public AccountType AccountType { get; set; }
+    
     [Required]
     [MinLength(1, ErrorMessage = "EmailAddress name cannot be empty.")]
     [EmailAddress]
     public required string EmailAddress { get; set; }
+    
+    [Required]
+    [Phone]
     public required string PhoneNumber { get; set; }
 }
